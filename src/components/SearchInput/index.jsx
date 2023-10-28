8612b5c8ad98068e0aea55cb7ce4bcbbffae4b52
JB <jadon.binda@gmail.com>
2023-10-28 13:55:22 -0300

    feat: implement heroes filter

--- src/components/SearchInput/index.jsx
+++ src/components/SearchInput/index.jsx
@@ -1,14 +1,26 @@
+import { Search } from '@mui/icons-material';
+import propTypes from 'prop-types';
 import React from 'react';
 import './styles.css';
-import { Search } from '@mui/icons-material';
 
-function SearchInput() {
+function SearchInput({ searchHero, setSearchHero }) {
   return (
     <form>
-      <Search sx={{ position: 'absolute', left: '8px', color: 'whitesmoke' }} />
-      <input type="search" name="search-input" className="search-input" />
+      <Search sx={{ position: 'absolute', top: '7px', left: '8px', color: 'whitesmoke' }} />
+      <input
+        type="search"
+        name="search-input"
+        className="search-input"
+        value={searchHero}
+        onChange={(event) => setSearchHero(event.target.value)}
+      />
     </form>
   );
 }
 
 export default SearchInput;
+
+SearchInput.propTypes = {
+  searchHero: propTypes.string,
+  setSearchHero: propTypes.string,
+}.isRequired;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header';
 import HeroCard from '../../components/HeroCard';
+import SearchInput from '../../components/SearchInput';
 import api from '../../services/api';
 import './styles.css';
 
 function Home() {
   const [allHeroes, setAllHeroes] = useState([]);
+  const [searchHero, setSearchHero] = useState('');
 
   async function loadHeroes() {
     try {
@@ -24,9 +26,17 @@ function Home() {
   return (
     <div className="container-main">
       <Header />
+      <section className="search-container">
+        <SearchInput searchHero={searchHero} setSearchHero={setSearchHero} />
+      </section>
       <section className="heroes-container">
         {
-          allHeroes.map((hero) => <HeroCard key={hero.id} hero={hero} />)
+          allHeroes.filter((hero) => {
+            const heroNameLowerCase = hero.name.toLocaleLowerCase();
+            const searchHeroLowerCase = searchHero.toLocaleLowerCase();
+            const heroNameFilter = heroNameLowerCase.includes(searchHeroLowerCase);
+            return heroNameFilter;
+          }).map((hero) => <HeroCard key={hero.id} hero={hero} />)
         }
       </section>
     </div>

773e361e7b00b65a4170ead1fc16fcdd6db9512f
JB <jadon.binda@gmail.com>
2023-10-28 01:32:00 -0300

    feat: implement functionality to list heroes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import './styles.css';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header';
+import HeroCard from '../../components/HeroCard';
+import api from '../../services/api';
+import './styles.css';
 
 function Home() {
+  const [allHeroes, setAllHeroes] = useState([]);
+
+  async function loadHeroes() {
+    try {
+      const response = await api.get('http://homologacao3.azapfy.com.br/api/ps/metahumans');
+
+      setAllHeroes([...response.data]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    loadHeroes();
+  }, []);
+
   return (
     <div className="container-main">
       <Header />
-      <h1>Hero Journey</h1>
+      <section className="heroes-container">
+        {
+          allHeroes.map((hero) => <HeroCard key={hero.id} hero={hero} />)
+        }
+      </section>
     </div>
   );
 }

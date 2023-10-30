4128ac61b01b64d64bd79345d8cfaf06dfd416fa
JB <jadon.binda@gmail.com>
2023-10-30 20:06:26 -0300

    feat: add a route to the main page

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Main from './pages/Main';
 
 function MainRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<Main />} />
       <Route path="/home" element={<Home />} />
     </Routes>
   );

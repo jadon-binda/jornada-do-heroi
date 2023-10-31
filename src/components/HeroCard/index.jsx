a2cb196ffbc6465b8af415a9e371a8103bbbe475
JB <jadon.binda@gmail.com>
2023-10-30 22:04:28 -0300

    style: add stylization to hero cards

--- src/components/HeroCard/index.jsx
+++ src/components/HeroCard/index.jsx
@@ -1,14 +1,47 @@
-import React from 'react';
-import './styles.css';
 import propTypes from 'prop-types';
+import React from 'react';
+import { PiSwordFill } from 'react-icons/pi';
+import './styles.css';
 
 function HeroCard({ hero, handleOpenModal }) {
   const { images, name, powerstats } = hero;
 
-  const sumPowerstats = () => {
+  const sumPowerStats = () => {
     const { intelligence, strength, speed, durability, power, combat } = powerstats;
     const combatPower = intelligence + strength + speed + durability + power + combat;
     return combatPower;
+  };
+
+  const cardColor = (sum) => {
+    if (sum > 0 & sum < 150) {
+      return { backgroundColor: 'rgba(0, 0, 139, 0.5)', border: 'solid rgba(135, 206, 235, 0.7)', borderWidth: '2px 1px 1px 2px' };
+    } else if (sum >= 150 & sum <= 250) {
+      return { backgroundColor: 'rgba(105, 105, 105, 0.3)', border: 'solid rgba(211, 211, 211, 0.7)', borderWidth: '2px 1px 1px 2px' };
+    } else if (sum > 250 & sum <= 350) {
+      return { backgroundColor: 'rgba(0, 100, 0, 0.5)', border: 'solid rgba(144, 238, 144, 0.5)', borderWidth: '2px 1px 1px 2px' };
+    } else if (sum > 350 & sum <= 500) {
+      return { backgroundColor: '	rgba(102, 0, 204, 0.3)', border: 'solid rgba(148, 0, 211, 0.7)', borderWidth: '2px 1px 1px 2px' };
+    } else if (sum > 500 & sum < 600) {
+      return { backgroundColor: 'rgba(218, 165, 32, 0.3)', border: 'solid rgba(255, 165, 0, 0.5)', borderWidth: '2px 1px 1px 2px' };
+    } else {
+      return { backgroundColor: 'rgba(139, 0, 0, 0.3)', border: 'solid rgba(255, 0, 0, 0.5)', borderWidth: '2px 1px 1px 2px' };
+    }
+  };
+
+  const cardShadow = (sum) => {
+    if (sum > 0 & sum < 150) {
+      return { boxShadow: '0px 2px 10px 2px rgba(135, 206, 235, 0.7)' };
+    } else if (sum >= 150 & sum <= 250) {
+      return { boxShadow: '0px 2px 10px 2px rgba(211, 211, 211, 0.7)' };
+    } else if (sum > 250 & sum <= 350) {
+      return { boxShadow: '0px 2px 10px 2px rgba(144, 238, 144, 0.5)' };
+    } else if (sum > 350 & sum <= 500) {
+      return { boxShadow: '0px 2px 10px 2px rgba(148, 0, 211, 0.7)' };
+    } else if (sum > 500 & sum < 600) {
+      return { boxShadow: '0px 2px 10px 2px rgba(255, 165, 0, 0.5)' };
+    } else {
+      return { boxShadow: '0px 2px 10px 2px rgba(255, 0, 0, 0.5)' };
+    }
   };
 
   const monitorarClique = (e) => {
@@ -31,19 +64,27 @@ function HeroCard({ hero, handleOpenModal }) {
   };
 
   return (
-    <section id="hero-card" className="hero-card" onClick={(e) => monitorarClique(e)}>
-      <img src={images.sm} alt={name} className="card__image" />
+    <section
+      id="hero-card"
+      className="hero-card"
+      onClick={(e) => monitorarClique(e)}
+      style={cardColor(sumPowerStats())}
+    >
+      <img
+        src={images.sm}
+        alt={name}
+        className="card__image"
+        style={cardShadow(sumPowerStats())}
+      />
       <div className="card__infos">
         <h2 className="card__name">{name}</h2>
-        <h3 className="card__power">{sumPowerstats()}</h3>
+        <h2 className="card__power"><PiSwordFill />{sumPowerStats()}</h2>
       </div>
     </section>
   );
 }
-HeroCard.propTypes;
 
 export default HeroCard;
-
 HeroCard.propTypes = {
   hero: propTypes.shape({}),
 }.isRequired;

77f9cbe961a7a4c078901f64b3939335256a9d8a
JB <jadon.binda@gmail.com>
2023-10-31 10:43:15 -0300

    feat: include and style text on main page

--- src/components/ContentMain/index.jsx
+++ src/components/ContentMain/index.jsx
@@ -1,13 +1,13 @@
-import { Typography, Stack, styled } from '@mui/material';
+import { Stack, Typography, styled } from '@mui/material';
 import React from 'react';
 import Herois from '../../assets/herois.png';
 
 const ContainerHeroes = styled(Stack)({
   width: '100%',
+  marginTop: 70,
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
-  justifyContent: 'center',
 });
 
 const MainTitle = styled(Typography)({
@@ -17,6 +17,12 @@ const MainTitle = styled(Typography)({
   textTransform: 'uppercase',
 });
 
+const MainText = styled(Typography)({
+  fontFamily: '\'Patrick Hand SC\', cursive',
+  fontSize: '1.25rem',
+  color: 'rgba(211, 211, 211, 0.8)',
+});
+
 function ContentMain() {
   return (
     <ContainerHeroes>
@@ -24,6 +30,7 @@ function ContentMain() {
       <MainTitle variant="h1">
         Jornada do Herói
       </MainTitle>
+      <MainText variant="body1">Clique no botão Cartas, ao lado, para iniciar o combate entre heróis...</MainText>
     </ContainerHeroes>
   );
 }

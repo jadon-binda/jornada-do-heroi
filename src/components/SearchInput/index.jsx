8bff89b33d83428bfe2bc9bcbf8ac284a0615591
JB <jadon.binda@gmail.com>
2023-10-31 13:21:16 -0300

    feat: add functionality and icon to clean the search input

--- src/components/SearchInput/index.jsx
+++ src/components/SearchInput/index.jsx
@@ -1,14 +1,26 @@
 import { Search } from '@mui/icons-material';
+import ClearIcon from '@mui/icons-material/Clear';
+import { Button } from '@mui/material';
 import propTypes from 'prop-types';
 import React from 'react';
 import styles from './styles.module.css';
 
 function SearchInput({ searchHero, setSearchHero }) {
+  function handleCleanSearchInput() {
+    setSearchHero('');
+  }
+
   return (
     <div className={styles.container}>
       <div>
         <form>
           <Search sx={{ position: 'absolute', top: '12px', left: '18px', color: 'whitesmoke' }} />
+          <Button
+            onClick={() => handleCleanSearchInput()}
+            sx={{ all: 'unset', position: 'absolute', top: '12px', right: '18px', cursor: 'pointer' }}
+          >
+            <ClearIcon sx={{ color: 'whitesmoke' }} />
+          </Button>
           <input
             type="search"
             name="search-input"
